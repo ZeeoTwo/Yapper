839cf7822fa2267ef9623bc694f1b7dfe0016c62
ZeeoTwo <koralgracjan@gmail.com>
2023-10-24 21:10:52 +0200

    Update yapper.py

--- Yapper/yapper.py
+++ Yapper/yapper.py
@@ -2,15 +2,16 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
-	print("Say something:")
-	audio = recognizer.listen(source)
 
 while True:
 	
 	try:
-		text = recognizer.recognize_google(audio, language="pl-PL")
-		print(f"You said: " + text) # + text.encode('utf-8').decode('utf-8')
+		with sr.Microphone() as source:
+			recognizer.adjust_for_ambient_noise(source, duration=0.2)
+			audio = recognizer.listen(source);
+			text = recognizer.recognize_google(audio, language="pl-PL")
+			text.lower();
+			print(f"You said: " + text) # + text.encode('utf-8').decode('utf-8')
 	except sr.UnknownValueError:
 		print("Google Web Speech API could not understand audio")
 	except sr.RequestError as e:
