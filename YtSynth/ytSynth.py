a5b7776c8c5122e2cdf3bb5cff9931afdcab1417
markusm1901 <122221692+markusm1901@users.noreply.github.com>
2023-11-01 17:21:39 +0100

    desktop app, added tkinter widgets, uploading a video or audio,

--- YtSynth/ytSynth.py
+++ YtSynth/ytSynth.py
@@ -7,102 +7,174 @@ from api import OPEN_AI_API_KEY
 import openai
 from docx import Document
 from docx2pdf import convert
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
 
 openai.api_key = OPEN_AI_API_KEY
 openai.Model.list()
 aai.settings.api_key = API_KEY
+class VideoNoteCreatorApp:
+    def __init__(self, root):
+        self.root = root
+        self.uploaded_file_path = None
+        self.root.title("YouTube Video Note Creator")
 
-def get_summary_openai(transcript):
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "make a note from given transcript, state a title of the transcription, state the most important points of the transcription, add to that note a detailed explanation to the scientific topics or subjects included in transcription: " + transcript}])
-    return response.choices[0].message.content
+        self.upload_label = ttk.Label(root, text="Upload an audio or a video file:")
+        self.upload_button = ttk.Button(root, text="upload", command=self.upload_file)        
+        self.yt_link_label = ttk.Label(root, text="YouTube link:")
+        self.yt_link_entry = ttk.Entry(root, width=50)
 
-def is_valid_lang(lang):
-    available_languages = ["en", "en_au", "en_uk", "en_us", "es", "fr", "de", "it", "pt", "nl", "hi", "ja", "zh", "fi", "ko", "pl", "ru", "tr", "uk", "vi"]
-    return lang in available_languages
+        self.lang_label = ttk.Label(root, text="Language of the video:")
+        self.lang_entry_combobox = ttk.Combobox(root, values=["en", "en_au", "en_uk", "en_us", "es", "fr", "de", "it", "pt", "nl", "hi", "ja", "zh", "fi", "ko", "pl", "ru", "tr", "uk", "vi"])
+        self.lang_entry_combobox.set("en")
 
-def is_valid_youtube_url(url):
-    try:
-        yt = YouTube(url)
-        return yt.title is not None
-    except Exception:
-        return False
+        self.file_format_label = ttk.Label(root, text="Type of file for your note:")
+        self.file_format_combobox = ttk.Combobox(root, values=["docx", "pdf", "txt", "md"])
+        self.file_format_combobox.set("docx")
 
-def is_valid_file_format(file_format):
-    available_types = ["docx", "pdf", "txt", "md"]
-    return file_format in available_types
+        self.process_button = ttk.Button(root, text="Process Video", command=self.download_and_process)
+        self.yt_link_entry.bind("<KeyRelease>", self.update_state)
 
-def create_note_file(file_format, note, file_name):
-    if file_format == "docx":
-        doc = Document()
-        doc.add_paragraph(note)
-        doc.save(file_name + ".docx")
-    if file_format == "pdf":
-        doc = Document()
-        doc.add_paragraph(note)
-        doc.save(file_name + ".docx")
-        convert(file_name + ".docx")
-        if os.path.exists(file_name + ".docx"):
-            os.remove(file_name + ".docx")
-    if file_format == "txt":
-        with open(file_name + ".txt", "w") as file_obj:
-            file_obj.write(note)
-    #markdown...
+        self.yt_link_label.grid(row=0, column=0, padx=10, pady=10)
+        self.yt_link_entry.grid(row=0, column=1, columnspan=2, padx=10, pady=10)
 
-def download_video(yt_link, lang, file_format):
-    while True:
-        if not is_valid_youtube_url(yt_link):
-            print("Your YouTube link is wrong")
-            yt_link = input("Please enter a valid YouTube link: ")
-            continue
-        if not is_valid_lang(lang):
-            print("Language code provided is wrong or not supported")
-            lang = input("Please enter a valid language code: ")
-            continue
-        if not is_valid_file_format(file_format):
-            print("Given file format is wrong or not supported")
-            file_format = input("Please enter a valid file format: ")
-            continue
-        youtube_obj = YouTube(yt_link)
-        video_file = youtube_obj.video_id + ".mp4"
-        audio_url = youtube_obj.video_id + ".mp3"
-        stream = youtube_obj.streams.get_highest_resolution()
-        stream.download(filename=video_file)
-        return video_file, audio_url, youtube_obj.video_id,lang, file_format
+        self.upload_label.grid(row=1,column=0,padx=10, pady=10)
+        self.upload_button.grid(row=1, column=1, padx=10, pady=10)
+        self.lang_label.grid(row=2, column=0, padx=10, pady=10)
+        self.lang_entry_combobox.grid(row=2, column=1, padx=10, pady=10)
 
-def extract_audio(video_file, audio_url):
-    video = VideoFileClip(video_file)
-    video.audio.write_audiofile(audio_url, codec="mp3")
-    video.close()
+        self.file_format_label.grid(row=3, column=0, padx=10, pady=10)
+        self.file_format_combobox.grid(row=3, column=1, padx=10, pady=10)
 
-def transcribe_audio(audio_url, lang):
-    cfg_transcription = aai.TranscriptionConfig(
-        language_code=lang,
-    )
-    transcriber = aai.Transcriber(config=cfg_transcription)
-    transcript = transcriber.transcribe(audio_url)
-    if transcript.text == "none":
-        print("Transcript not available")
-        return None
-    return transcript
+        self.process_button.grid(row=4, column=0, columnspan=3, padx=10, pady=10)
+        
+    def update_state(self,event):
+        if self.yt_link_entry.get():
+            self.upload_button.config(state="disabled")
+        else:
+            self.upload_button.config(state="normal")
+    def reset_values(self):
+        self.yt_link_entry.delete(0,tk.END)
+        self.yt_link_entry.insert(0,"")
+        self.lang_entry_combobox.set("en")
+        self.file_format_combobox.set("docx")
+        self.uploaded_file_path = ""
+
+    def download_and_process(self):
+        lang = self.lang_entry_combobox.get()
+        file_format = self.file_format_combobox.get()
+        if self.uploaded_file_path:
+            self.yt_link_entry.config(state="disabled")
+            self.upload_button.config(state="normal")
+            transcript = self.transcribe_audio(self.uploaded_file_path,lang)
+            if transcript: 
+                note = self.get_summary_openai(transcript.text)
+                if not self.create_note_file(file_format, note, self.uploaded_file_path):
+                    messagebox.showinfo("Success", "Note file created successfully.")
+                    self.yt_link_entry.config(state ="normal")
+                    self.upload_button.config(state = "normal")
+                    self.reset_values()
+
+                else:
+                    messagebox.showerror("Error", "Failed to create the note file.")
+
+        if  self.yt_link_entry.get():
+            self.yt_link_entry.config(state="normal")
+            self.upload_button.config(state="disabled")
+            yt_link = self.yt_link_entry.get()
+            video_file, audio_url, file_name, lang, correct_file_format = self.download_video(yt_link, lang, file_format)
+            if video_file and audio_url:
+                self.extract_audio(video_file, audio_url)
+                transcript = self.transcribe_audio(audio_url, lang)
+                if transcript:
+                    note = self.get_summary_openai(transcript.text)
+                    self.create_note_file(correct_file_format, note, file_name)
+                    if os.path.exists(audio_url):
+                        os.remove(video_file)
+                        os.remove(audio_url)
+                        messagebox.showinfo("Success", "Note file created successfully.")
+                        self.yt_link_entry.config(state ="normal")
+                        self.upload_button.config(state = "normal")
+                        self.reset_values()
+                    else:
+                        messagebox.showerror("Error", "Failed to create the note file.")
+
+    def get_summary_openai(self,transcript):
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "make a note from given transcript, state a title of the transcription, state the most important points of the transcription, add to that note a detailed explanation to the scientific topics or subjects included in transcription: " + transcript}])
+        return response.choices[0].message.content
+
+    def is_valid_youtube_url(self, url):
+        try:
+            yt = YouTube(url)
+            return yt.title is not None
+        except Exception:
+            return False
+
+    def download_video(self, yt_link, lang, file_format):
+        while True:
+            if not self.is_valid_youtube_url(yt_link):
+                messagebox.showerror("Error", "Provided youtube link is either nonexisted or unavailable.")
+                break
+            else:
+                youtube_obj = YouTube(yt_link)
+                video_file = youtube_obj.video_id + ".mp4"
+                audio_url = youtube_obj.video_id + ".mp3"
+                stream = youtube_obj.streams.get_highest_resolution()
+                stream.download(filename=video_file)
+                return video_file, audio_url, youtube_obj.video_id, lang, file_format
+
+    def create_note_file(self,file_format, note, file_name):
+        if file_format == "docx":
+            doc = Document()
+            doc.add_paragraph(note)
+            doc.save(file_name + ".docx")
+        if file_format == "pdf":
+            doc = Document()
+            doc.add_paragraph(note)
+            doc.save(file_name + ".docx")
+            convert(file_name + ".docx")
+            if os.path.exists(file_name + ".docx"):
+                os.remove(file_name + ".docx")
+        if file_format == "txt":
+            with open(file_name + ".txt", "w") as file_obj:
+                file_obj.write(note)
+
+    def extract_audio(self,video_file, audio_url):
+        video = VideoFileClip(video_file)
+        video.audio.write_audiofile(audio_url, codec="mp3")
+        video.close()
+
+    def transcribe_audio(self,audio_url, lang):
+        cfg_transcription = aai.TranscriptionConfig(
+            language_code=lang,
+        )
+        transcriber = aai.Transcriber(config=cfg_transcription)
+        transcript = transcriber.transcribe(audio_url)
+        if transcript.text == "none":
+            messagebox.showerror("Error", "Failed to create the note file.")
+            return None
+        return transcript
+    
+    def upload_file(self):
+        self.uploaded_file_path = filedialog.askopenfilename(
+        title="Select a Video or Audio File",
+        filetypes=[
+            ("Video Files", "*.mts *.m2ts *.ts *.mov *.mp2 *.mp4 *.m4v *.mxf *.webm"),
+            ("Audio Files", "*.8svx *.aac *.ac3 *.aif *.aiff *.alac *.amr *.ape *.au *.dss *.flac *.flv *.m4a *.m4b *.m4p *.m4r *.mp3 *.mpga *.ogg *.oga *.mogg *.opus *.qcp *.tta *.voc *.wav *.wma *.wv"),
+        ])
+        self.upload_button.config(state="disabled")
+        self.yt_link_entry.config(state ="disabled")
+
 
 def main():
-    yt_link = input("YouTube link: ")
-    yt_lang = input("Language of the video: ")
-    file_format = input("Type of file for your note: ")
-    video_file, audio_url, file_name, lang, correct_file_format = download_video(yt_link, yt_lang, file_format)
-    if video_file and audio_url:
-        extract_audio(video_file, audio_url)
-        transcript = transcribe_audio(audio_url, lang)
-        if transcript:
-            note = get_summary_openai(transcript.text)
-            create_note_file(correct_file_format, note, file_name)
-            if os.path.exists(audio_url):
-                os.remove(video_file)
-                os.remove(audio_url)
-
+    app = tk.Tk()
+    VideoNoteCreatorApp(app)
+    app.mainloop()
 if __name__ == "__main__":
     main()
